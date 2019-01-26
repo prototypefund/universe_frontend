5079a6a0e4bf4eb9411da60439eea8bb25129a08
Nic Zemke <nic@transparency-everywhere.com>
2019-01-26 21:33:40 +0100

    login html added to dock

--- src/components/Dock.vue
+++ src/components/Dock.vue
@@ -1,5 +1,21 @@
 <template>
-  <div id="dock">Dock</div>
+  <div id="dock">
+    <div class="box-shadow" id="startbox" style="z-index: 9999; position: absolute;">
+                <form method="post" target="submitter" id="loginForm" onsubmit="login(); return false;">
+                    <table valign="center" width="100%">
+                        <tbody><tr valign="top">
+                            <td><input type="text" placeholder="username" id="loginUsername"></td>
+                        </tr>
+                        <tr>
+                            <td><input type="password" placeholder="password" id="loginPassword"></td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" value="Log In" name="submit" class="button pull-right"></td>
+                        </tr>
+                    </tbody></table>
+                </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,5 +39,26 @@ export default {
   margin-bottom: 0px;
   padding-bottom: 0px;
   background: #000;
+}
+#startbox{
+  position: absolute;
+  bottom: 40px;
+  left: 0px;
+  background-color: #607d8b;
+  box-shadow: 1px 3px 8px 0px rgba(0, 0, 0, 0.3);
+  width: 360px;
+  height: 190px;
+  padding: 20px;
+}
+#startbox input[type="text"], #startbox input[type="password"] {
+
+    width: 100%;
+    height: 40px;
+    padding: 7px;
+    font-size: 16px;
+    color: #313131;
+    margin-bottom: 15px;
+    border: none;
+
 }
 </style>
