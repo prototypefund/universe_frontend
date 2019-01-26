2c785c997f994b7ec9cc31ffaa916a02c8d0d33a
Nic Zemke <nic@transparency-everywhere.com>
2019-01-26 21:30:10 +0100

    crypto util improved

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -8,9 +8,51 @@ import {
 
 
 var cry = function(){
-    this.generateAsymKeys = function(){
+
+    this.symEncrypt = function(json, key){
+      let keyHash = this.hash(key,'');
+      console.log('keyHash');
+      console.log(keyHash = keyHash.slice(0, 32));
+      console.log(keyHash.length);
+
+
+
+      //const keyUint8Array = decodeUTF8(keyHash);
+      const nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+      const messageUint8 = decodeUTF8(JSON.stringify(json));
+      const box = nacl.secretbox(messageUint8, nonce, keyHash);
+
+      const fullMessage = new Uint8Array(nonce.length + box.length);
+      fullMessage.set(nonce);
+      fullMessage.set(box, nonce.length);
+
+      const base64FullMessage = encodeBase64(fullMessage);
+      return base64FullMessage;
+    };
+
+    //password is used to encrypt secretKey
+    this.generateUserKeys = function(password){
+
         const keyPair = nacl.box.keyPair();
-        return keyPair;
+        //@sec for debugging purpose
+
+        //generate random salt for hashing the password
+        const randomSalt = 'asdagskdhasdad';
+
+        //hash password
+        const passwordHash = this.hash(password,randomSalt);
+
+        const encryptedSecretKey = this.symEncrypt(keyPair.secretKey,password);
+        
+        const encryptedSalt = this.symEncrypt(randomSalt,password);
+        
+
+        return {
+          publicKey:encodeBase64(keyPair.publicKey),
+          encryptedSecretKey:encryptedSecretKey,
+          encryptedSalt:encryptedSalt,
+          passwordHash:encodeBase64(passwordHash)
+        }
     }
     this.test = function(){
         const keyAlice = nacl.box.keyPair();
@@ -30,7 +72,13 @@ var cry = function(){
           const plaintext = nacl.box.open(ciphertext, nonce, keyAlice.publicKey, keyBob.secretKey)
           console.log(encodeUTF8(plaintext));
 
-    }
+    };
+    this.hash = function(string, salt){
+      //@sec for debugging purpose
+      //return string+'_'+salt;
+      return nacl.hash(decodeUTF8(string+salt));
+
+    };
 }
 
 export default new cry();
