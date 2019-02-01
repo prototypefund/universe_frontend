6608f73c0c0b4f118cb4b7dabd0a0810885b8cc6
Nic Zemke <nic@transparency-everywhere.com>
2019-02-01 01:57:21 +0100

    login function with jwt token added to dock

--- src/components/Dock.vue
+++ src/components/Dock.vue
@@ -1,29 +1,84 @@
 <template>
-  <div id="dock">
-    <div class="box-shadow" id="startbox" style="z-index: 9999; position: absolute;">
+  <div>
+    <div id="loginBox" v-if="!auth">
                 <form method="post" target="submitter" id="loginForm" onsubmit="login(); return false;">
-                    <table valign="center" width="100%">
-                        <tbody><tr valign="top">
-                            <td><input type="text" placeholder="username" id="loginUsername"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="password" placeholder="password" id="loginPassword"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Log In" name="submit" class="button pull-right"></td>
-                        </tr>
-                    </tbody></table>
+                  <div>
+                    <input type="text" placeholder="username" v-model="username">
+                  </div>
+                  <div>
+                    <input type="password" placeholder="password" v-model="password">
+                  </div>
+                  <div>
+                      <universeButton text="Login" :click="login" style="float: right;margin-right: 0;"></universeButton>
+                  </div>
                 </form>
+    </div>
+    <div id="dock">
+      <universeButton text="Login" :click="toggleLogin" v-if="!auth"></universeButton>
+      <universeButton text="Apps" :click="login"></universeButton>
     </div>
   </div>
 </template>
 
 <script>
+
+import $ from 'jquery';
+import UniverseButton from '@/components/UniverseButton'
+import api from '../utils/api'
+import cry from '../utils/crypto'
+import { authBus } from '../main';
+
+
 export default {
   name: 'Dock',
+  components: {
+    UniverseButton
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      auth:false,
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login : function(){
+      alert('Login!');
+        const self = this;
+        const username = this.username;
+        api.get('user/getUserSalt/'+username,{
+        },function(err,result,body){
+          if(err){
+            if(error.error == 'no_user_found')
+              alert('No matching user found');
+          }
+          else{
+            
+            const passwordSalt = cry.symDecrypt(body.encryptedSalt,self.password);
+
+            const passwordHash = cry.encodeBase64(cry.hash(self.password,passwordSalt));
+
+
+            api.post('user/login', {
+              username,
+              passwordHash
+            },(error,result,body)=>{
+              if(error)
+                console.log(error)
+              else{
+                authBus.$emit('auth', {
+                  user:body.user,
+                  jwt:body.jwt
+                });
+                self.auth = true;
+              }
+            });
+          }
+          
+        });
+    },
+    toggleLogin : function(){
+      $('#loginBox').slideToggle();
     }
   }
 }
@@ -40,23 +95,22 @@ export default {
   padding-bottom: 0px;
   background: #000;
 }
-#startbox{
+#loginBox{
   position: absolute;
   bottom: 40px;
   left: 0px;
   background-color: #607d8b;
   box-shadow: 1px 3px 8px 0px rgba(0, 0, 0, 0.3);
-  width: 360px;
-  height: 190px;
-  padding: 20px;
-}
-#startbox input[type="text"], #startbox input[type="password"] {
 
-    width: 100%;
+  height: 190px;
+  padding: 25px;
+}
+#loginBox input[type="text"], #loginBox input[type="password"] {
+
+    width: 320px;
     height: 40px;
     padding: 7px;
     font-size: 16px;
-    color: #313131;
     margin-bottom: 15px;
     border: none;
 
