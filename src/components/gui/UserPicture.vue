7cdad39c9328ce77e3702f0b221d27f74a62a74d
niczem <nic@sea-watch.org>
2019-02-20 00:42:15 +0100

    userpicture component added

---
+++ src/components/gui/UserPicture.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="userpicture standarduser" v-bind:style="{ width: size + 'px', height: size + 'px', borderRadius:size/2+'px' }">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPicture',
+  props: ['userid', 'size'],
+  mounted: function () {
+      console.log('modal ready');
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.userpicture{
+    background-position: center center;
+    background-size: cover!important;
+    background-repeat: no-repeat;
+    border: 2px solid;
+    float: left;
+    cursor: pointer;
+}
+.standarduser {
+    background-image: url(../../assets/gfx/standarduser.png);
+}
+</style>
