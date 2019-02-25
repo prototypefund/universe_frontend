272ceac6432aafec959c7563e6fcaf6ed81d81de
niczem <nic@sea-watch.org>
2019-02-25 21:00:32 +0100

    addBudy() & setConf() implemented

--- src/utils/user.js
+++ src/utils/user.js
@@ -17,6 +17,46 @@ var user = function(){
           });
       })
     }
+    this.acceptFriendRequest = function(user_id, request_id){
+
+      //get old buddylist
+
+      //push user to it
+
+      //encrypt and store it
+
+      
+    }
+    this.deleteFriendRequest = function(request_id){
+      return new Promise((resolve, reject)=>{
+        api.post('requests/delete',{
+            request_id:request_id
+          },function(err,result,body){
+            if(err){
+              reject(err);
+            }
+            else{
+              resolve(body);
+            }
+          });
+      })
+    }
+    this.setConfig = function(index, value){
+      return new Promise((resolve, reject)=>{
+          api.post('user/setConfig',{
+            index:index,
+            value:value
+          },function(err,result,body){
+            if(err){
+              reject(err);
+            }
+            else{
+              resolve(body);
+            }
+          });
+      })
+    }
+
 }
 
 export default new user();
