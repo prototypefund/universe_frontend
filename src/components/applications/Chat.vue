789fdf5c8d8fe89f4b71cd7a7739f039dc94069a
niczem <nic@sea-watch.org>
2019-02-28 06:23:09 +0100

    chat component added

---
+++ src/components/applications/Chat.vue
@@ -0,0 +1,43 @@
+<template>
+  <tabs>
+    <tab name="Home" :selected="true">
+      <div>
+        <center>
+            <span class="icon blue-comment" style="height: 90px;width: 90px; margin-top:5px;margin-right: -17px;"></span>
+            <h2 style="margin-top:0;">Chat</h2>
+            <h3>Click on a user in your buddylist to open a dialogue</h3>
+        </center>
+      </div>
+    </tab>
+  </tabs>
+</template>
+<script>
+import UniverseButton from '@/components/gui/UniverseButton'
+import api from '@/utils/api'
+
+import Tabs from '@/components/gui/Tabs'
+import Tab from '@/components/gui/Tab'
+
+export default {
+  name: 'Chat',
+  components:{
+    UniverseButton,
+    Tabs,
+    Tab
+  },
+  
+
+  data () {
+    return {
+      name:'',
+    }
+  },
+  methods:{
+    submit:function(){
+      
+    }
+  },
+  created:function(){
+  }
+}
+</script>
