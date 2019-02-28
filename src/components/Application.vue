656fc74fa899721540b10d41266a7d7916278be9
niczem <nic@sea-watch.org>
2019-02-28 06:02:20 +0100

    icons added, handling improved

--- src/components/Application.vue
+++ src/components/Application.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <div class="applicationWindow" :style="styleObject">
-      <header>{{ title }}</header>
+      <header>
+        <span class="title">{{ title }}</span>
+        <span class="applicationIcons">
+          <a @click="minimize">
+            <span class="icon  dark-icon dark-minimize"></span>
+            <span class="icon blue-icon blue-minimize"></span>
+          </a>
+          <a @click="setToFullScreen">
+            <span class="icon dark-icon dark-maximize"></span>
+            <span class="icon blue-icon blue-maximize"></span>
+          </a>
+          <a @click="minimize">
+            <span class="icon dark-icon dark-close"></span>
+            <span class="icon blue-icon blue-close"></span>
+          </a>
+        </span>
+      </header>
       <div class="content">
         <component :is="component"></component>
       </div>
@@ -41,16 +57,22 @@ export default {
   methods: {
     initStyles (styles) {
       const windowWidth = window.innerWidth;
-      const windowHeight = window.innerHeight;
+      const windowHeight = window.innerHeight-40; //remove height of dock
       if(this.styles){
         if(this.styles.width)
           this.styleObject.width = this.styles.width*(windowWidth/12)+'px';
         if(this.styles.left)
           this.styleObject.left = this.styles.left*(windowWidth/12)+'px';
+        else
+          this.styleObject.left = 0;
         if(this.styles.right)
           this.styleObject.right = this.styles.right*(windowWidth/12)+'px';
+        else
+          this.styleObject.right = 0;
         if(this.styles.top)
           this.styleObject.top = this.styles.top*(windowHeight/4)+'px';
+        else
+          this.styleObject.top = 0;
         if(this.styles.height)
           this.styleObject.height = this.styles.height*(windowHeight/4)+'px';
         if(this.styles.hidden)
@@ -65,7 +87,7 @@ export default {
       
       $(".applicationWindow").not('.ui-draggable').draggable({
               cancel: '.inhalt',
-              containment: '#app',
+              containment: '#desktop',
               scroll: false,
               drag: function(){
                   //disable textmarking
@@ -87,6 +109,8 @@ export default {
                           //bring window to front 
                           $(this).css('z-index', 9999);
                           $(this).css('position', 'absolute');
+                          $(this).css('position', 'absolute');
+                          $(this).css('opacity', 1);
                   }
               },
               stop: function(){
@@ -102,6 +126,7 @@ export default {
             $(".applicationWindow").css('z-index', 999);
             $(this).parent(".applicationWindow").css('z-index', 9999); 
             $(this).parent(".applicationWindow").css('position', 'absolute');
+            $(this).parent(".applicationWindow").css('opacity', 1);
           
             $('.applicationWindow').removeClass('onTop');
             $(this).parent(".applicationWindow").addClass('onTop');
@@ -110,6 +135,19 @@ export default {
       });
 
 
+    },
+    minimize:function(){
+      this.styles.hidden = true;
+      this.styleObject.display = 'none';
+      this.$forceUpdate();
+    },
+    setToFullScreen:function(){
+      this.styles.width = 12;
+      this.styles.height = 4;
+      this.styles.top = 0;
+      this.styles.right = 0;
+      this.styles.left = 0;
+      this.initStyles(this.styles);
     }
   },
   mounted: function () {
@@ -140,12 +178,27 @@ export default {
     cursor: move;
     height: 60px;
     overflow: none;
-    padding: 20px 0 0 15px;
+    padding: 15px 15px 0 15px;
     text-overflow: ellipsis;
     font-size: 24px;
     white-space: nowrap;
     box-sizing: border-box;
     border-bottom: 1px solid #dcdcdc;
+}
+
+.applicationWindow > header .applicationIcons{
+  float:right;
+}
+.applicationWindow > header .applicationIcons a{
+  cursor:pointer;
+  float:left;
+}
+
+.applicationWindow > header .applicationIcons a .blue-icon,.applicationWindow > header .applicationIcons a:hover .dark-icon{
+  display:none;
+}
+.applicationWindow > header .applicationIcons a:hover .blue-icon{
+  display:block;
 }
 
 .applicationWindow > .content{
