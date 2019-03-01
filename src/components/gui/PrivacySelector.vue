a73ea215abdf428f00ee8cebba79c9e6c36a6315
niczem <nic@sea-watch.org>
2019-03-01 01:23:38 +0100

    css added

--- src/components/gui/PrivacySelector.vue
+++ src/components/gui/PrivacySelector.vue
@@ -59,5 +59,111 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.privacySettings {
+                    color: #313131;
+                }
+                
+        .privacySettings h1, .privacySettings h2, .privacySettings h3{
+          margin: 0;
+          line-height:15px;
+        }
+        
+        .privacySettings h2{
+          line-height: 23px;
+          font-size: 16px;
+                                        display: inline-block;
+        }
+        
+        .privacySettings > header{
+                                        color: #FFF;
+          position: relative;
+          font-size: 16px;
+          line-height: 30px;
+                                        margin-bottom: 10px;
+                                }
+        
+        .privacySettings ul{
+        }
+        
+        .privacySettings>ul>li{
+                                    margin-bottom: 12px;
+                                    padding: 3px;
+                                    padding-left: 35px;
+                                    clear: both;
+                                    height: 40px;
+                                    line-height: 36px;
+                                    color: #313131;
+                                    background-color: #ffffff;
+                                    margin-bottom: 5px;
+                                    cursor: pointer;
+        }
+                                
+                                .privacySettings>ul>li .white-check{
+                                    display:none;
+                                }
+                                
+        .privacySettings>ul>li.active{
+                                    background-color:  #00bcd4;
+                                    color:#FFF;
+                                    padding-left: 2px;
+                                }
+                                
+                                .privacySettings>ul>li.active .white-check{
+                                    display: inline-block;
+                                    margin-right: 2px;
+                                    height: 30px;
+                                    width: 30px;
+                                    margin-bottom: -8px;
+                                }
+                                
+                                .privacySettings h2 input[type=checkbox]{
+                                    display:none;
+                                }
+                                
+        
+        .privacySettings input[type=checkbox]{
+          margin: -3.5px 1px 0 -13px;
+        }
+        
+        .privacySettings .sub{
+          margin-left: 25px;
+                                        border-top: none;
+                                        display: none;
+                                        height: 40px;
+                                        padding-left: 35px;
+                                        padding-top: 0px;
+                                }
+        
+        .privacySettings .sub > div{
+                                        width:45%;
+                                        height: 35px;
+                                        font-size: 18px;
+                                        display: inline-block;
+        
+                                        vertical-align: middle;
+}
+        
+        .privacySettings .sub > div:first-of-type{
+          margin-left: 0;
+        }
+        
+        .privacySettings .groupList li{
+          clear: both;
+          padding-left: 5px;
+        
+          min-height: 30px;
+}
+        
+        .privacySettings .groupList li > div:first-of-type{
+          margin-left: 0;
+          width: 120px;
+        }
+        
+        .privacySettings .groupList li > div{
+          margin-left: 20px;
+                                        
+          float: left;
+        
+          display: block;
+}
 </style>
